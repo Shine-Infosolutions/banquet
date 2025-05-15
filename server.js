c23da84282dae2f1dbe2c4d2607316d4e795e48b
Aatif Sahir <atifsha@outlook.com>
2025-05-15 14:49:09 +0530

    server file code change

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log("Error connecting to MongoDB:", err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
 
 // Use routes
 app.use("/api", bookingRoutes);
