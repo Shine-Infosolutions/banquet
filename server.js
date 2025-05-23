592f4184a8e7cedec2663389119707feabcdcc08
Aatif Sahir <atifsha@outlook.com>
2025-05-23 15:59:35 +0530

    menuCategories added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const bookingRoutes = require("./routes/bookingRoutes");
 const menuRoutes = require("./routes/menuRoutes");
+const menuCategoryRoutes = require("./routes/menuCategoryRoutes");
 const connectDB = require("./db/db");
 
 const app = express();
@@ -17,7 +18,7 @@ connectDB();
 // Use routes
 app.use("/api", bookingRoutes);
 app.use("/api", menuRoutes);
-
+app.use("/api/categories", menuCategoryRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
