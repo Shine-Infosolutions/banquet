5c6eadc38c5ca86996ba749152409253483c8a20
Aatif Sahir <52101985+atif-shaheer@users.noreply.github.com>
2025-05-24 03:02:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ connectDB();
 app.use("/api", bookingRoutes);
 app.use("/api", menuRoutes);
 app.use("/api/categories", menuCategoryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello API IS LIVE");
+});
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
