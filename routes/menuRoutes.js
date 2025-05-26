ad902d71bae8c1350121a5d057b6737c3e2d5bc5
Aatif Sahir <atifsha@outlook.com>
2025-05-26 11:18:58 +0530

    m

--- routes/menuRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const menuController = require("../controllers/menuController");
-const upload = require("../middleware/upload");
-
-router.post("/menu-items", upload.single("image"), menuController.addMenuItem);
-router.get("/menu-items", menuController.getAllMenuItems);
-
-module.exports = router;
