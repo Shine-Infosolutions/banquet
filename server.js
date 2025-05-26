f635f76c4afca0be5c98a3b66a8c857bbe743fef
Aatif Sahir <atifsha@outlook.com>
2025-05-26 11:23:26 +0530

    server

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const bookingRoutes = require("./routes/bookingRoutes");
-const menuRoutes = require("./routes/menuRoutes");
-const menuCategoryRoutes = require("./routes/menuCategoryRoutes");
 const connectDB = require("./db/db");
 
 const app = express();
@@ -13,12 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 connectDB();
-
-
-// Use routes
-app.use("/api", bookingRoutes);
-app.use("/api", menuRoutes);
-app.use("/api", menuCategoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello API IS LIVE");
